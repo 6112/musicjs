8e5d94bfe64dbf211ef8622dbbec7394a942a7d4
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-04 13:37:37 -0400

    Fix build errors.

--- src/client/data/playlist.ts
+++ src/client/data/playlist.ts
@@ -3,7 +3,7 @@ import { Track } from './track';
 export class Playlist {
   public static deserialize(json: string): Playlist {
     const parsed = JSON.parse(json);
-    const tracks = parsed.tracks.map((track) => JSON.stringify(Track.deserialize(track)));
+    const tracks = parsed.tracks.map((track: any) => Track.deserialize(JSON.stringify(track)));
     return new Playlist(parsed.name, tracks);
   }
 
@@ -13,7 +13,7 @@ export class Playlist {
   public serialize(): string {
     return JSON.stringify({
       name: this.name,
-      tracks: this.tracks.map((track) => track.serialize)
+      tracks: this.tracks.map((track: Track) => track.serialize())
     });
   }
 }
