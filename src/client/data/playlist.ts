94b2e97221344847afa7fc58e37f533b34842604
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-04 13:51:03 -0400

    Fixed test for playlist

--- src/client/data/playlist.ts
+++ src/client/data/playlist.ts
@@ -3,7 +3,7 @@ import { Track } from './track';
 export class Playlist {
   public static deserialize(json: string): Playlist {
     const parsed = JSON.parse(json);
-    const tracks = parsed.tracks.map((track: any) => Track.deserialize(JSON.stringify(track)));
+    const tracks = parsed.tracks.map((track: any) => Track.deserialize(track));
     return new Playlist(parsed.name, tracks);
   }
 
