8ca65672a958d50f65e476b4514fe68da4b98d32
Nicolas <ashlebede@gmail.com>
2017-11-04 13:16:35 -0400

    wip

--- src/client/components/search-component.ts
+++ src/client/components/search-component.ts
@@ -1,3 +1,18 @@
-export class SearchComponent {
+type TimerId = number;
+type Callback = () => void;
 
+const debouncers = new Map<string, TimerId>();
+
+export function debounce(callback: Callback, delay: number,
+                         debouncerName: string) {
+  if (debouncers.has(debouncerName)) {
+    window.clearTimeout(debouncers.get(debouncerName));
+  }
+  const timeout = window.setTimeout(callback, delay);
+  debouncers.set(debouncerName, timeout);
+}
+
+export class SearchComponent {
+  constructor() {
+  }
 }
