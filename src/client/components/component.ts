cbd299418d325db0dcbc2a3e20976605cb3c8d62
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-06 10:36:21 -0500

    doc

--- src/client/components/component.ts
+++ src/client/components/component.ts
@@ -6,13 +6,27 @@ export interface Component {
   show(): void;
 }
 
+/**
+ * Base of the UI component.
+ */
 export class BaseComponent {
+  /**
+   * Constructor.
+   * @param id ID of the component. Must be unique.
+   * @param title Title of the component.
+   */
   public constructor(public id: string, public title: string) { }
 
+  /**
+   * Displays the UI component.
+   */
   public show(): void {
     document.getElementById(this.id).removeAttribute('hidden');
   }
 
+  /**
+   * Hides the UI component.
+   */
   public hide(): void {
     document.getElementById(this.id).setAttribute('hidden', '');
   }
