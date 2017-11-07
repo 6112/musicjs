aea7f4b1d7b0449ce29ffc6934dd8176ea0bd97a
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-06 21:14:26 -0500

    'fixing' server

--- src/server/server.js
+++ src/server/server.js
@@ -1,38 +1,33 @@
 #!/usr/bin/node
 
-const express = require("express")
-const path = require("path")
+const express = require('express');
+const path = require('path');
 
-const spotify = require("./lib/spotify")
+const spotify = require('./lib/spotify');
 
-const PORT = 3000
+const PORT = 3000;
 
 function init() {
-  const app = express()
+  const app = express();
 
-  app.get("/spotify_auth_token", (req, res) => {
-    spotify.getAuthToken().then(token => {
-      res.send(token)
-    }).catch(err => {
-      res.status(500).send(err.message)
-    })
-  })
+  app.get('/spotify_auth_token', (req, res) => {
+    spotify.getAuthToken().then((token) => {
+      res.send(token);
+    }).catch((err) => {
+      res.status(500).send(err.message);
+    });
+  });
 
-  app.use(
-    express.static(path.resolve(__dirname, "public")))
-  app.use(
-    express.static(path.resolve(__dirname, "../../dist/browser")))
+  app.use(express.static(path.resolve(__dirname, 'public')));
+  app.use(express.static(path.resolve(__dirname, '../../dist/browser')));
 
   app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "public/index.html"))
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
   });
 
   app.listen(PORT, () => {
-    console.log(`Listening on localhost:${PORT}`)
-  })
+    console.log(`Listening on localhost:${PORT}`);
+  });
 }
 
-init()
-
-
-
+init();
