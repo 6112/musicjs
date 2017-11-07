87b8d2951377036946f6db0e06edffb6d1ecaca5
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-07 14:06:34 -0500

    More tests

--- src/client/managers/api/fetch.spec.ts
+++ src/client/managers/api/fetch.spec.ts
@@ -1,19 +1,27 @@
 import { MockFetcher } from './fetch';
 
 QUnit.module('MockFetcher');
-QUnit.test('basic example', async (t) => {
+
+QUnit.test('basic example', async (assert) => {
   const mock = new MockFetcher();
   const r = new Response('404 - Not Found', { status: 404 });
   mock.pushResponse(r);
-  t.equal(await mock.fetch('/'), r);
+  assert.deepEqual(await mock.fetch('/'), r);
 });
 
-QUnit.test('two fetches in a row', async (t) => {
+QUnit.test('two fetches in a row', async (assert) => {
   const mock = new MockFetcher();
   const r1 = new Response('404 - Not Found', { status: 404 });
   const r2 = new Response('200 - OK', { status: 200 });
   mock.pushResponse(r1);
   mock.pushResponse(r2);
-  t.equal(await mock.fetch('/'), r1);
-  t.equal(await mock.fetch('/foo.html'), r2);
+  assert.deepEqual(await mock.fetch('/'), r1);
+  assert.deepEqual(await mock.fetch('/foo.html'), r2);
+});
+
+QUnit.test('should return 500 when there are no responses', async (assert) => {
+  const mock = new MockFetcher();
+  const result = await mock.fetch('/');
+  const internalErrorCode = 500;
+  assert.deepEqual(result.status, internalErrorCode);
 });
--- src/client/managers/player-manager.spec.ts
+++ src/client/managers/player-manager.spec.ts
@@ -6,24 +6,27 @@ import { Playlist } from '../data/playlist';
 QUnit.module('PlayerManager');
 
 QUnit.test('should play a track', (assert) => {
-  const done = assert.async();  
+  const done = assert.async();
   const src = 'https://e-cdns-preview-4.dzcdn.net/stream/4957d8283d8face656a373f3b18087fa-1.mp3';
   const trackLength = 326;
   const track = new Track('Lose Yourself (Soundtrack Version)', 'Eminem', trackLength, src, Provider.DEEZER);
   const audio = new Audio();
+  const playLength = 30000;
+  const bufferTime = 10000;
+  const startTime = new Date().getTime();
   audio.addEventListener('ended', () => {
     const endTime = new Date().getTime();
-    assert.ok(endTime - startTime >= 30000);
-    assert.ok(endTime - startTime <= 31000);
+    assert.ok(endTime - startTime >= playLength);
+    assert.ok(endTime - startTime <= playLength + bufferTime);
     assert.deepEqual(src, audio.src);
     done();
   });
-  const startTime = new Date().getTime();  
   PlayerManager.playTrack(track, audio);
 });
 
 QUnit.test('should play a playlist', (assert) => {
-  const done = assert.async(2);
+  const numberOfExpectedDones = 2;
+  const done = assert.async(numberOfExpectedDones);
   const src1 = 'https://e-cdns-preview-4.dzcdn.net/stream/4957d8283d8face656a373f3b18087fa-1.mp3';
   const trackLength1 = 326;
   const track1 = new Track('Lose Yourself (Soundtrack Version)', 'Eminem', trackLength1, src1, Provider.DEEZER);
@@ -32,16 +35,18 @@ QUnit.test('should play a playlist', (assert) => {
   const track2 = new Track('Look What You Made Me Do', 'Taylor Swift', trackLength2, src2, Provider.DEEZER);
   const playlist = new Playlist('Eminem', [track1, track2]);
   const audio = new Audio();
+  const playLength = 30000;
+  const bufferTime = 10000;
+  let currentSrc = src1;
+  let startTime = new Date().getTime();
   audio.addEventListener('ended', () => {
     const endTime = new Date().getTime();
-    assert.ok(endTime - startTime >= 30000);
-    assert.ok(endTime - startTime <= 31000);
+    assert.ok(endTime - startTime >= playLength);
+    assert.ok(endTime - startTime <= playLength + bufferTime);
     assert.deepEqual(currentSrc, audio.src);
     currentSrc = src2;
     startTime = new Date().getTime();
     done();
   });
-  let currentSrc = src1;
-  let startTime = new Date().getTime();
   PlayerManager.playPlaylist(playlist, audio);
 });
