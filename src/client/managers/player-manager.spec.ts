bd8312cd2003a79e76d5aa0e0ad22ecd7d7ec944
Jovan El Hoayek <jhoayek@gmail.com>
2017-11-09 13:42:06 -0500

    MockAudio for faster tests.

--- src/client/managers/player-manager.spec.ts
+++ src/client/managers/player-manager.spec.ts
@@ -1,7 +1,31 @@
-import { PlayerManager } from './player-manager';
+import { PlayerManager, Audio } from './player-manager';
 import { Provider } from '../data/provider';
 import { Track } from '../data/track';
 import { Playlist } from '../data/playlist';
+import { Emitter } from '../components/emitter';
+
+class MockAudio extends Emitter implements Audio  {
+  public static DELAY = 3000;
+  
+  public src: string;
+  private timeout: NodeJS.Timer;
+
+  play(): void {
+    this.timeout = setTimeout(() => {
+      this.dispatchEvent(new CustomEvent('ended'));
+    }, MockAudio.DELAY);
+  }
+
+  pause(): void {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
+
+  addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void {
+    super.addEventListener(type, listener);
+  }
+}
 
 QUnit.module('PlayerManager');
 
@@ -10,8 +34,8 @@ QUnit.test('should play a track', (assert) => {
   const src = 'https://e-cdns-preview-4.dzcdn.net/stream/4957d8283d8face656a373f3b18087fa-1.mp3';
   const trackLength = 326;
   const track = new Track('Lose Yourself (Soundtrack Version)', 'Eminem', trackLength, src, Provider.DEEZER);
-  const audio = new Audio();
-  const playLength = 30000;
+  const audio = new MockAudio();
+  const playLength = MockAudio.DELAY;
   const bufferTime = 10000;
   const startTime = new Date().getTime();
   audio.addEventListener('ended', () => {
@@ -34,8 +58,8 @@ QUnit.test('should play a playlist', (assert) => {
   const trackLength2 = 211;
   const track2 = new Track('Look What You Made Me Do', 'Taylor Swift', trackLength2, src2, Provider.DEEZER);
   const playlist = new Playlist('Eminem', [track1, track2]);
-  const audio = new Audio();
-  const playLength = 30000;
+  const audio = new MockAudio();
+  const playLength = MockAudio.DELAY;
   const bufferTime = 10000;
   let currentSrc = src1;
   let startTime = new Date().getTime();
